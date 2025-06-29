a41bf34ccb65b82d0109380c3583c518070766e3
LiaCBA <moonlightt1083@gmail.com>
2025-06-29 13:25:32 +0400

    typescript

--- 02-TypeScript.ts
+++ 02-TypeScript.ts
@@ -2,11 +2,13 @@
 
 // მაგალითი 1
 
-let name = "Alice";
+let name1 = "Alice";
+let name2: string = "Alice";
 
 // მაგალითი 2
 
 let numbers = [1, 2, 3];
+let numbers2: number[] = [1, 2, 3];
 
 // მაგალითი 3
 
@@ -14,21 +16,32 @@ function multiply(a, b) {
   return a * b;
 }
 
+function multiply1(a: number, b: number): number {
+  return a * b;
+}
+
 // მაგალითი 4
 
-const user = { id: 1, name: "Alice" };
+const user1 = { id: 1, name: "Alice" };
+const user2: { id: number; name: string } = { id: 1, name: "Alice" };
 
 // მაგალითი 5
 // რისთვის ვიყენებთ არსებული კოდში = Guest-ს :
 
 function greet(name: string = "Guest") {}
 
-// პასუხი:
+// პასუხი: თუ greet-ს ვიძახებთ სახელის გარეშე, მაშინ ავტომატურად "Guest" გადაეცემა
 
 // მაგალითი 6
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type Config = {
 
-const config1 = {
+type ConfigProps = {
+  theme?: string;
+  options?: { fontSize: number; layout: null };
+  tester?: string;
+};
+
+const config1: ConfigProps = {
   theme: "dark",
   options: {
     fontSize: 16,
@@ -36,19 +49,28 @@ const config1 = {
   },
 };
 
-const config2 = {
+const config2: ConfigProps = {
   theme: "dark",
 };
 
-const config3 = {
+const config3: ConfigProps = {
   tester: "test",
 };
 
 // მაგალითი 7
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type MixedArray =
-const array1 = [42, "hello", { name: "Alice" }];
-const array2 = ["apple", true, { isValid: false }];
-const array3 = [];
+
+type MixedArrayProps = (
+  | number
+  | string
+  | { name: string }
+  | boolean
+  | { isValid: boolean }
+)[];
+
+const array1: MixedArrayProps = [42, "hello", { name: "Alice" }];
+const array2: MixedArrayProps = ["apple", true, { isValid: false }];
+const array3: MixedArrayProps = [];
 
 // მაგალითი 8
 // აღწერეთ რისი ტიპიზაცია ხდება არსებულ კოდში წერილობით
@@ -63,9 +85,16 @@ type Handler = {
 
 // შექმენით ტიპი მონაცემისთივს:
 
-type User = {};
+type UserProps = {
+  id: number;
+  username: string;
+  isAdmin: boolean;
+  profile: { fullName: string; age: number; interests: string[] };
+  settings: { theme: string; notifications: { email: boolean; sms: boolean } };
+  metadata: undefined;
+};
 
-const user: User = {
+const user: UserProps = {
   id: 101,
   username: "tech_learner",
   isAdmin: false,
